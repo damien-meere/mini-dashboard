27e16e0d5e1b933782b30c7b3bd1d80fe4fb726c
Damien Meere <damien.meere@bt.com>
2019-09-29 12:23:53 +0000

    Scatter Plot added to show correlation betwen years since PhD and salary

--- static/js/graph.js
+++ static/js/graph.js
@@ -5,14 +5,14 @@ queue()
 function makeGraphs(error, salaryData) {
     var ndx = crossfilter(salaryData);
 
-
     // Before we get into actually plotting the data, we're going to loop over all of the data in the salaryData data set.
     // And we're going to convert the salaries to integers.
     salaryData.forEach(function(d){
         d.salary = parseInt(d.salary);
-
         // We wrap yrs.service  in the square bracket and quotes, rather than using the dot notation because years of service actually has a dot in it, so it would cause problems
         d.yrs_service = parseInt(d["yrs.service"]);
+        d.yrs_since_phd = parseInt(d["yrs.since.phd"]);
+
     });
 
     show_discipline_selector(ndx);
@@ -25,6 +25,7 @@ function makeGraphs(error, salaryData) {
     show_rank_distribution(ndx);
 
     show_service_to_salary_correlation(ndx);
+    show_phd_to_salary_correlation(ndx);
 
     dc.renderAll();
 }
@@ -238,4 +239,40 @@ function show_service_to_salary_correlation(ndx){
         .dimension(experienceDim)
         .group(experienceSalaryGroup)
         .margins({top:10, right: 50, bottom: 75, left: 75});
+}
+
+
+function show_phd_to_salary_correlation(ndx){
+
+    var genderColors = d3.scale.ordinal()
+        .domain(["Female", "Male"])
+        .range(["red", "blue"]);
+
+    var pDim = ndx.dimension(dc.pluck("yrs_since_phd"));
+    var phdDim = ndx.dimension(function(d) {
+       return [d.yrs_since_phd, d.salary, d.rank, d.sex];
+    });
+    var phdSalaryGroup = phdDim.group();
+
+    var minPhd = pDim.bottom(1)[0].yrs_since_phd;
+    var maxPhd = pDim.top(1)[0].yrs_since_phd;
+
+    dc.scatterPlot("#phd-salary")
+        .width(800)
+        .height(400)
+        .x(d3.scale.linear().domain([minPhd, maxPhd]))
+        .brushOn(false)
+        .symbolSize(8)
+        .clipPadding(10)
+        .xAxisLabel("Years Since PhD")
+        .title(function(d) {
+            return d.key[2] + " earned " + d.key[1];
+        })
+        .colorAccessor(function (d) {
+            return d.key[3];
+        })
+        .colors(genderColors)
+        .dimension(phdDim)
+        .group(phdSalaryGroup)
+        .margins({top: 10, right: 50, bottom: 75, left: 75});
 }
